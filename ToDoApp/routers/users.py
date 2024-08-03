adb1a3fe90e398178539bdd8b0094ccd38e072e9
mdshadanaltmash <mdshadanaltmash44@gmail.com>
2024-08-03 21:00:22 +0530

    alembic and creating phone_number using alembic

--- ToDoApp/routers/users.py
+++ ToDoApp/routers/users.py
@@ -5,7 +5,7 @@ from passlib.context import CryptContext
 from passlib.handlers.bcrypt import bcrypt
 from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
-from starlette.status import HTTP_200_OK, HTTP_202_ACCEPTED
+from starlette.status import HTTP_200_OK, HTTP_202_ACCEPTED, HTTP_204_NO_CONTENT
 
 from database import SessionLocal
 from models import Todos, Users
@@ -58,6 +58,18 @@ async def change_password(user: user_dependency, db: db_dependency, user_verific
         raise HTTPException(status_code=401, detail='Old password is incorrect')
 
     user_info.hashed_password = bcrypt_context.hash(user_verification.new_password)
+
+    db.add(user_info)
+    db.commit()
+
+
+@router.put('/phone_number/{phone_number}', status_code=HTTP_204_NO_CONTENT)
+async def change_phone_number(user: user_dependency, db: db_dependency, phone_number: str):
+    if not user:
+        raise HTTPException(status_code=401, detail='Not Authenticated')
+
+    user_info = db.query(Users).filter(Users.id == user.get('id')).first()
+    user_info.phone_number = phone_number
 
     db.add(user_info)
     db.commit()
